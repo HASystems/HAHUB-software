0d2d75a7b09081fed483dcc9d487a8508eb3ca39
Atul Varshneya <atul.varshneya@gmail.com>
2015-12-31 13:10:51 -0800

    Commented haconfig.py to explain the file format and describe all the API functions.

--- etc/hahub/hapkg/haconfig.py
+++ etc/hahub/hapkg/haconfig.py
@@ -2,6 +2,45 @@
 
 class Config:
 
+	####################################################################################
+	# Utility for maintaining the config information of multiple modules in one place and 
+	# sharing it among various HAHUB mudues via a configuration object
+	# 
+	# Very simple file format:
+	#     * No leading spces or tabs are allowed
+	#     * Comment line - starts with a # charater, the entire line is ignored
+	#     * Blank (empty) lines are allowed to help format the information visually into groups
+	#     * key=value pairs, one per line. The characters in the lione before the '=' character
+	#         constitute the key name, all text after constitutes its value.
+	# Example:
+	# # this is a comment
+	# NUMBER_OF_THREADS=10
+	# DELAY=0.25
+	# LOG_FILE=/var/log/hahub/hahubd.log
+	# # Another comment
+	# 
+	#
+	# API Functions --
+	#
+	# readconfig() 
+	#         Takes as a parameter a config filename. As of current implementation it should be 
+	#         called only once for one config object. Easy to change to support multiple calls 
+	#         each adding/overwriting config parameter values.
+	#
+	# getConfigValue(key, default_value) - returns a string value for the key.
+	#         default_value must be a string. It is returned if key name is not found.
+	# 
+	# getConfigInt(key, default_value) - returns an integer value for the key.
+	#         default_value must be an integer. It is returned if key name is not found.
+	# 
+	# getConfigFloat(key, default_value) - returns a floating point value for the key.
+	#         default_value must be a floating point number. It is returned if key name is not found.
+	# 
+	# setConfigValue(key, value) - makes an entry of this pair in the config object.
+	#         Does NOT make this entry in the config file. Just a maeans to share
+	#         this new pair with other modules. This function is not used often.
+	####################################################################################
+
 	def __init__(self):
 		self.configmap = {}
 
