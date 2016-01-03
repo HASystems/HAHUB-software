c76a4fd1db60074bd6a619a6f08f40aa468ab2ac
Atul Varshneya <atul.varshneya@gmail.com>
2016-01-03 12:55:43 -0800

    Lists the commads as defined in ha.rc files, and executes those commands on click. Always stays on the same home page through redirect

--- client/webremote/webapp.py
+++ client/webremote/webapp.py
@@ -1,52 +1,49 @@
 import os
 import syslog
 from flask import Flask, render_template
-import hapkg.haconfig
-import hapkg.hacmdapi
+import haconfig
+import hacmdapi
 
 app = Flask(__name__)
 
 @app.route('/hacmdapi/v1.3/tasks/<task_id>', methods=['GET'])
 def get_task(task_id):
 	global cmdapi
+	syslog.syslog(syslog.LOG_INFO, "Command - %s" % task_id)
+	print "Command - %s" % task_id
 	cmdapi.runlist([task_id],"at Top Level")
-	return render_template('cmdhome.html', commands=cmdapi.oper_list())
+	# return render_template('cmdhome.html', commands=sortedcmds(cmdapi.oper_list()))
+	return redirect('/hacmdapi/v1.3/tasks/', code=302)
 
 @app.route('/hacmdapi/v1.3/tasks/', methods=['GET'])
 @app.route('/')
 def index():
-	syslog.syslog(syslog.LOG_INFO, "home page")
-	print render_template('cmdhome.html', commands=cmdapi.oper_list())
-	return render_template('cmdhome.html', commands=cmdapi.oper_list())
+	global cmdapi
+	syslog.syslog(syslog.LOG_INFO, "Home Page")
+	print "Home Page"
+	return render_template('cmdhome.html', commands=sortedcmds(cmdapi.oper_list()))
 
-@app.route('/power')
-def power():
-	syslog.syslog(syslog.LOG_INFO, "power page")
-	cmdlist = ["allon"]
-	cmdapi.runlist(cmdlist,"at Top Level")
-	return render_template('power.html')
+@app.route('/hacmdapi/v1.3/test/', methods=['GET'])
+def test_page():
+	global cmdapi
+	syslog.syslog(syslog.LOG_INFO, "Test Page")
+	print "Test Page"
+	# print render_template('test.html', commands=sortedcmds(cmdapi.oper_list()))
+	return render_template('test.html', commands=sortedcmds(cmdapi.oper_list()))
 
-@app.route('/volumeup')
-def volup():
-	syslog.syslog(syslog.LOG_INFO, "volup page")
-	cmdlist = ["v+"]
-	cmdapi.runlist(cmdlist,"at Top Level")
-	return render_template('volumeup.html')
-
-@app.route('/volumedown')
-def voldown():
-	syslog.syslog(syslog.LOG_INFO, "voldn page")
-	cmdlist = ["v-"]
-	cmdapi.runlist(cmdlist,"at Top Level")
-	return render_template('volumedown.html')
+def sortedcmds(cmdgrplist):
+	sortedlist = {}
+	for g in cmdgrplist.keys():
+		sortedlist[g] = sorted(cmdgrplist[g])
+	return sortedlist
 
 syslog.openlog("haweb",0,syslog.LOG_LOCAL0)
 syslog.setlogmask(syslog.LOG_UPTO(syslog.LOG_INFO))
 
-config = hapkg.haconfig.Config()
+config = haconfig.Config()
 config.readConfig("/etc/hahub/hahubd.conf")
 
-cmdapi = hapkg.hacmdapi.HacmdAPI()
+cmdapi = hacmdapi.HacmdAPI()
 cmdapi.setconfig(config)
 
 rcfiles = ["/etc/hahub/ha.rc", "~/.ha.rc", "./ha.rc", "~/bin/ha.rc"]
