343f81254f5527e05f95e4df784d3c8d9f57ea1b
Atul Varshneya <atul.varshneya@gmail.com>
2016-01-03 18:27:24 -0800

    Corrected an error in install.sh - incorrect source p[ath for rc.local

--- install.sh
+++ install.sh
@@ -67,7 +67,7 @@ rm -rf $LIBDIR/*
 echo "Copying files from ${SDIR}/halib to $LIBDIR ..."
 cp -r $SDIR/halib/* $LIBDIR
 
-echo "Copying $SDIR/dev/etc/rc.local to /etc/rc.local"
-cp $SDIR/dev/etc/rc.local /etc/rc.local
+echo "Copying $SDIR/etc/rc.local to /etc/rc.local"
+cp $SDIR/etc/rc.local /etc/rc.local
 
 echo "Done"
